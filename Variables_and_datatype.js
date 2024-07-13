97a821fe55bbdab4ad9fa0fd1446247f740b8e16
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-07-13 06:56:47 +0000

    first chellenges completed

--- Variables_and_datatype.js
+++ Variables_and_datatype.js
@@ -1,1 +1,34 @@
+// var num1 = 100;
+// console.log(num1);
+
+// let user = "jonny";
+// console.log(user);
+
+// const bool = true;
+// console.log(bool);
+
+// Creates variable of different datatypes (number,strings,boolean,Array,Object) and log each variables type using the typeof operator.
+var num1 = 100;
+let user = "jonny";
+console.log(user);
+const bool = true;
+console.log(bool);
+let array1 = [2,4,5,"a"];
+let obj = {
+    user:"sachin",
+    age:21
+}
+
+// console.log(typeof num1);
+// console.log(typeof user);
+// console.log(typeof bool);
+// console.log(typeof array1);
+// console.log(typeof obj);
+
+user = "tommy";
+console.log(user);
+bool = false;
+// console.log(bool);           //Error
+
+
 
