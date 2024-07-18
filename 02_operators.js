2724e39f2f2dc9875d9fd33069689d4c788c3bed
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-07-18 15:17:16 +0000

    day-2 solve the questions

--- 02_operators.js
+++ 02_operators.js
@@ -1,1 +1,71 @@
+/*
+// Add Two numbers  
+let num1 = 10;
+let num2 = 20;
+let result = num1+num2;
+console.log(result);
 
+// Substract Two numbers
+ num1 = 20;
+ num2 = 10;
+ result = num1-num2;
+console.log(result);
+
+//multiply Two numbers
+num1 = 20;
+num2 = 10;
+result = num1*num2;
+console.log(result);
+
+// find reminders 
+num1 = 24;
+num2 = 10;
+result = num1%num2;
+console.log(result);
+
+
+// # User the += operator to add a number to a variable and the log the result to the console.
+
+let number = 15 ;
+number += 10;
+console.log(number);
+
+// # User the -= operator to subtract a number from a variable and the log the result to the console.
+
+number = 14;
+number -= 5;
+console.log(number);
+
+
+console.log(4>9);
+console.log(4<9);
+
+
+console.log(4<=9);
+console.log(4>=9);
+
+console.log(5 == 6);
+console.log(9===9);
+
+console.log(5 == "5");              //true
+console.log(5 === "5");             //false
+
+
+*/
+
+console.log(6>10 && 7>10);              //FALSE
+console.log(6<10 && 7>10);              //FALSE
+console.log(6<10 && 7<10);               //TRUE
+
+
+
+console.log(6>10 || 7>10);              //FALSE
+console.log(6<10 || 7>10);              //TRUE
+console.log(6<10 || 7<10);               //TRUE\
+
+
+
+console.log(!(4<12));                   //false
+
+let num1 = -12;
+num1 > 0 ? console.log("Positive") : console.log("Negative");
