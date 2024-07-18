b06287f0f43f95427293732ef46eb97984fcbd26
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-07-18 18:24:18 +0000

    Day-3 complete challenges

---
+++ 03_control_structure.js
@@ -0,0 +1,98 @@
+// let number = 0;
+// let number = -20;
+// let number = 10;
+// if(number > 0){
+//     console.log("Positive Number");
+// }else if(number < 0){
+//     console.log("Negative Number");
+// }else{
+//     console.log(" Number is Zero");
+// }
+
+
+
+
+// let age = 12;
+// age >= 18 ? console.log("Eligible for vote!") : console.log("Not Eligible!") ;
+
+
+//# write a program to find the largest of three numbers using nested if-else statement.
+
+// let num1 =560;
+// let num2 =310;
+// let num3 = 179;
+
+// if(num1 >= num2){
+//     if(num1 >= num3){
+//         console.log(num1);
+//     }else{
+//         console.log(num3);
+//     }
+// }else if(num1 <= num2){
+//     if(num2 >= num3){
+//         console.log(num2);
+//     } else{
+//         console.log(num3);
+//     }
+// }
+
+
+
+// let day = new Date().getDay();
+// console.log("Day number:",day);
+// switch (day+1) {
+//     case 1:
+//         console.log("Today is Sunday");
+//         break;
+//     case 2:
+//         console.log("Today is Monday");
+//         break;
+//     case 3:
+//         console.log("Today is Tuesday");
+//         break;
+//     case 4:
+//         console.log("Today is Wednesday");
+//         break;
+//     case 5:
+//         console.log("Today is Thursday");
+//         break;
+//     case 6:
+//         console.log("Today is Friday");
+//         break;
+//     case 7:
+//         console.log("Today is Saturday");
+//         break;
+//     default:
+//         console.log("Enter valid day number");
+//         break;
+// }
+
+
+
+// let marks = 68;
+// switch (true) {
+//         case (marks >= 75):
+//             console.log("Your Grade is A");
+//             break;
+//         case (marks >= 67):
+//             console.log("Your Grade is B");
+//             break;
+//         case (marks >= 48):
+//             console.log("Your Grade is C");
+//             break;
+//         case (marks >= 33):
+//             console.log("Your Grade is D");
+//             break;
+//         default:
+//             console.log("Your Grade is F");
+//             break;
+//     }
+
+
+
+
+// let num = 121;
+
+// (num % 2 === 0) ? console.log("Even"):console.log("Odd");
+
+
