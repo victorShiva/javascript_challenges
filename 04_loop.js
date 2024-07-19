9029b05c75fa26d53f8473bdbddd9d5041db0db0
shiva gupta <shivagupta5607@gmail.com>
2024-07-19 08:03:15 +0530

    4th day challenge is completed

---
+++ 04_loop.js
@@ -0,0 +1,68 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log("i", i);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(5 * i);
+// }
+
+
+
+
+//# write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+// let i = 1;
+// let sum = 0;
+// while (i <= 10) {
+//     sum += i;
+//     i++;
+// }
+// console.log(sum);
+
+
+
+// let i = 10;
+// while (i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+
+
+// let num = 4;
+// let factorial = 1;
+// do {
+//     factorial = factorial * num;
+//     num--;
+// } while (num >= 1)
+// console.log(factorial);
+
+
+// for (let i = 0; i < 5; i++) {
+//     let star = "";
+//     for (let k = 0; k <= i; k++) {
+//         star += " * ";
+//     }
+//     console.log(star);
+// }
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 5) continue;
+//     console.log(i);
+// }
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i == 7) break;
+// }
